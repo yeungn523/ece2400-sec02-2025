448c0597ca6c8d6da4f26f488ac847b4eed69a7c
matth2k <mrh259@cornell.edu>
2025-02-02 14:13:21 -0500

    refresh code for 2025

---
+++ src/square-adhoc.c
@@ -0,0 +1,9 @@
+#include "square.h"
+#include <stdio.h>
+
+int main() {
+  int a = 10;
+  int b = square(a);
+  printf("square of %d is %d\n", a, b);
+  return 0;
+}
--- src/square.c
+++ src/square.c
@@ -1,6 +1,5 @@
 #include "square.h"
 
-int square( int x )
-{
+int square(int x) {
   // insert code to calculate square here
 }
